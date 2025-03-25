7fc158158748562f8022f99ecce035389509b19c
D. Galagan <dima@airtrail.co>
2025-03-25 09:23:48 +0100

    removed chat icon

--- app/dashboard/layout.jsx
+++ app/dashboard/layout.jsx
@@ -16,7 +16,6 @@ import {
   X,
   Home,
   Settings,
-  MessageSquare,
 } from "lucide-react";
 import { typography } from "../styles/design-system";
 import { signOut, useSession } from "next-auth/react";
@@ -28,13 +27,11 @@ const adminNavigation = [
   { name: "Orders", href: "/dashboard/orders", icon: ShoppingBag },
   { name: "Inventory", href: "/dashboard/products", icon: Package },
   { name: "Customer Base", href: "/dashboard/customers", icon: Users },
-  { name: "Chat", href: "/chat", icon: MessageSquare },
   { name: "Settings", href: "/dashboard/settings", icon: Settings },
 ];
 
 const userNavigation = [
   { name: "My Purchases", href: "/dashboard/orders", icon: ShoppingBag },
-  { name: "Chat", href: "/chat", icon: MessageSquare },
   { name: "Settings", href: "/dashboard/settings", icon: Settings },
 ];
 
